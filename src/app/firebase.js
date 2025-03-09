f4b675c79979bf3a1b686199e1d693e2d2c2a1f9
reginaliew <liew.reginamh@gmail.com>
2025-03-09 14:17:39 +0800

    update firebase

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_judCAXEyyTtDSoBQn_RlwqdEbTJd8Yg",
-  authDomain: "portfolio-fb64a.firebaseapp.com",
-  projectId: "portfolio-fb64a",
-  storageBucket: "portfolio-fb64a.appspot.com",
-  messagingSenderId: "1083418238231",
-  appId: "1:1083418238231:web:e19c94740d87bf8ea12b52",
-  measurementId: "G-GFX0KLQZSB"
+  apiKey: "AIzaSyCjQYnvF4CcuNw1rJffijSGLD24TE_4o-Q",
+  authDomain: "portfolio-reginaliew.firebaseapp.com",
+  projectId: "portfolio-reginaliew",
+  storageBucket: "portfolio-reginaliew.firebasestorage.app",
+  messagingSenderId: "446155391665",
+  appId: "1:446155391665:web:7900953024a1da63b61890",
+  measurementId: "G-CHR1MCGZEE"
 };
 
 // Initialize Firebase
